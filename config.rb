2efb47bfd590bc10aafc376c65bed7963f485773
Carson Shold <carson@carsonshold.com>
2014-04-28 15:35:22 -0400

    Added config.rb for compass. Updated to v1.2.0. Removed hard coded stylesheets

---
+++ config.rb
@@ -0,0 +1,17 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "css"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
