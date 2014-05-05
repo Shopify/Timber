9ae98e5183eaff800e25e25fed1c4184bae57ca9
Jonathon Halliwell <jon@bantros.net>
2014-05-05 02:09:34 +0100

    Update ajaxify.js to extend $cartCountSelector
    $cartCountSelector should update using the correct case now, it was previously showing '1 items'

--- assets/ajaxify.js
+++ assets/ajaxify.js
@@ -350,7 +350,17 @@ var ajaxifyShopify = (function(module, $) {
 
   updateCountPrice = function (cart) {
     if ($cartCountSelector) {
-      $cartCountSelector.html(cart.item_count);
+      switch(cart.item_count) {
+        case 0:
+          $cartCountSelector.html('0 items');
+          break;
+        case 1:
+          $cartCountSelector.html('1 item');
+          break;
+        default:
+          $cartCountSelector.html('" + cart.item_count + " items');
+          break;
+      }
     }
     if ($cartCostSelector) {
       $cartCostSelector.html(Shopify.formatMoney(cart.total_price, settings.moneyFormat));
