347b32212b936a781d481913599ed8dec237ac83
Carson Shold <carson@carsonshold.com>
2014-07-11 16:00:02 -0400

    Added back latest pr

--- assets/ajaxify.js
+++ assets/ajaxify.js
@@ -677,6 +677,8 @@ var ajaxifyShopify = (function(module, $) {
         buildCart(cart);
         if ( !$drawerContainer.hasClass('is-visible') ) {
           showDrawer();
+        } else {
+          scrollTop();
         }
         break;
     }
