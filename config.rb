6e3f5236e01b213b05f42917cd80b004531738d1
Carson Shold <carson@carsonshold.com>
2015-01-07 13:24:51 -0500

    Version bump. Compress output css. Update timber css

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ images_dir = "images"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :nested
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
